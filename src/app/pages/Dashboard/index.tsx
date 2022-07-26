d83344c0e3bad95fe47059ab1f375ac530346de6
joallisson <joallissonnc@gmail.com>
2022-07-26 09:03:05 -0300

    feat: Aula 06 - Usando o useState da forma certa

--- src/app/pages/Dashboard/index.tsx
+++ src/app/pages/Dashboard/index.tsx
@@ -1,17 +1,30 @@
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export const Dashboard = () => {
 
-    const navigate = useNavigate(); //usando hook de navegação entre páginas
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-    const handleClick = () => {
-        navigate("/entrar");
+    const handleEntrar = () => {
+        console.log(`Email: ${email}   Password: ${password}`);
     }
 
     return(
         <div>
-            <p>Dashboard</p>
-            <button onClick={handleClick}>Entrar</button>
+            <form action="">
+                <label htmlFor="">
+                    <span>Email</span>
+                    <input value={email} onChange={e => setEmail(e.target.value)} type="email" />
+                </label>
+
+                <label htmlFor="">
+                    <span>Senha</span>
+                    <input onChange={e => setPassword(e.target.value)} value={password} type="password" />
+                </label>
+
+                <button onClick={handleEntrar} type="button">Entrar</button>
+            </form>
+
         </div>
     );
 }
