117488f42469d9e0c6238d914e60d9e1d72c7497
Joallisson Nascimento Castro <69011012+Joallisson@users.noreply.github.com>
2022-07-27 19:35:51 -0300

    Aula 11 - Usando o useRef com ebjetos simples

--- src/app/pages/Dashboard/index.tsx
+++ src/app/pages/Dashboard/index.tsx
@@ -1,57 +1,21 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useRef } from "react";
+import { Link } from "react-router-dom";
+
 
 export const Dashboard = () => {
 
-    const inputPasswordRef = useRef<HTMLInputElement>(null);
-
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-
-    // useEffect(() => {
-    //     if (window.confirm("Você é homem?")) {
-    //         console.log("Homem");
-    //     } else {
-    //         console.log("Mulher");
-    //     }
-    // }, []);
-
-    const emailLength = useMemo(() => {
-        console.log("Executou");
-        return email.length * 1000;
-    }, [email.length]);
-
-    const handleEntrar = useCallback(() => {
-        console.log(email);
-        console.log(password);
-    }, [email, password]);
+    const counterRef = useRef(0);
 
     return(
         <div>
-            <p>Quantidade de caracteres no email: {emailLength}</p>
-            <form action="">
-                <label htmlFor="">
-                    <span>Email</span>
-                    <input 
-                        value={email} 
-                        onChange={e => setEmail(e.target.value)} 
-                        type="email"
-                        onKeyDown={e => e.key === 'Enter' ? inputPasswordRef.current?.focus() : undefined}
-                    />
-                </label>
+           <p>Dashboard</p>
 
-                <label htmlFor="">
-                    <span>Senha</span>
-                    <input
-                        ref={inputPasswordRef}
-                        onChange={e => setPassword(e.target.value)} 
-                        value={password} 
-                        type="password" 
-                    />
-                </label>
+           <p>Contador: {counterRef.current}</p>
 
-                <button onClick={handleEntrar} type="button">Entrar</button>
-            </form>
+           <button onClick={() => counterRef.current++}>Somar</button>
+           <button onClick={() => console.log(counterRef.current)}>Log</button>
 
+           <Link to="/entrar">Login</Link>
         </div>
     );
 }
--- src/app/pages/Login/index.tsx
+++ src/app/pages/Login/index.tsx
@@ -1,17 +1,57 @@
-import { useNavigate } from "react-router-dom";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 
 export const Login = () => {
 
-    const navigate = useNavigate(); //usando hook de navegação entre páginas
+    const inputPasswordRef = useRef<HTMLInputElement>(null);
 
-    const handleClick = () => {
-        navigate("/pagina-inicial");
-    }
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    // useEffect(() => {
+    //     if (window.confirm("Você é homem?")) {
+    //         console.log("Homem");
+    //     } else {
+    //         console.log("Mulher");
+    //     }
+    // }, []);
+
+    const emailLength = useMemo(() => {
+        console.log("Executou");
+        return email.length * 1000;
+    }, [email.length]);
+
+    const handleEntrar = useCallback(() => {
+        console.log(email);
+        console.log(password);
+    }, [email, password]);
 
     return(
         <div>
-            <p>Login</p>
-            <button onClick={handleClick}>Home</button>
+            <p>Quantidade de caracteres no email: {emailLength}</p>
+            <form action="">
+                <label htmlFor="">
+                    <span>Email</span>
+                    <input 
+                        value={email} 
+                        onChange={e => setEmail(e.target.value)} 
+                        type="email"
+                        onKeyDown={e => e.key === 'Enter' ? inputPasswordRef.current?.focus() : undefined}
+                    />
+                </label>
+
+                <label htmlFor="">
+                    <span>Senha</span>
+                    <input
+                        ref={inputPasswordRef}
+                        onChange={e => setPassword(e.target.value)} 
+                        value={password} 
+                        type="password" 
+                    />
+                </label>
+
+                <button onClick={handleEntrar} type="button">Entrar</button>
+            </form>
+
         </div>
     );
 }
