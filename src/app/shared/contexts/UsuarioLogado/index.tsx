15a77d50348d92fa7e52c74f8827be331f67f22f
Joallisson Nascimento Castro <69011012+Joallisson@users.noreply.github.com>
2022-08-09 19:50:25 -0300

    Aula 19 - Passando state no contexto

--- src/app/shared/contexts/UsuarioLogado/index.tsx
+++ src/app/shared/contexts/UsuarioLogado/index.tsx
@@ -1,4 +1,4 @@
-import { createContext, useCallback } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
 
 interface IUsuarioLogadoContextData{
     nomeDoUsuario: string;
@@ -13,12 +13,20 @@ export const UsuarioLogadoContext = createContext<IUsuarioLogadoContextData>({} 
 
 export const UsuarioLogadoProvider: React.FC<IUsuarioLogadoProps> = ({ children }) => {
 
+    const [nome, setNome] = useState('testes');
+
+    useEffect(() => {
+        setTimeout(() => {
+            setNome('Joallisson');
+        }, 3000);
+    }, []);
+
     const handleLogout = useCallback(() => {
         console.log('Logout executou');
     }, []);
 
     return (
-        <UsuarioLogadoContext.Provider value={{ nomeDoUsuario: 'Lucas', logout: handleLogout}}>
+        <UsuarioLogadoContext.Provider value={{ nomeDoUsuario: nome, logout: handleLogout}}>
             { children }
         </UsuarioLogadoContext.Provider>
     );
