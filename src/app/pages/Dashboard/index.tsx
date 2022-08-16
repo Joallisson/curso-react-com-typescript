17c73de8e57be3c3c81e0a09c6fde4a79fc88f62
Joallisson Nascimento Castro <69011012+Joallisson@users.noreply.github.com>
2022-08-16 20:41:10 -0300

    Aula 20 - Trabalhando com listas

--- src/app/pages/Dashboard/index.tsx
+++ src/app/pages/Dashboard/index.tsx
@@ -1,28 +1,34 @@
-import { useRef } from "react";
-import { Link } from "react-router-dom";
-import { useUsuarioLogado } from "../../shared/hooks";
-
+import { useCallback, useState } from "react";
 
 export const Dashboard = () => {
 
-    const counterRef = useRef(0);
+    const [lista, setLista] = useState<string[]>(["teste1", "teste2", "teste3"]);
 
-    const { nomeDoUsuario, logout } = useUsuarioLogado();
+    const handleInputKeyDown: React.KeyboardEventHandler<HTMLInputElement> = useCallback((e) => {
+        if (e.key === 'Enter') {
+            if (e.currentTarget.value.trim().length === 0) return;
+
+            const value = e.currentTarget.value;
+            e.currentTarget.value = '';
+
+            setLista((oldLista) => {
+                if(oldLista.includes(value)) return oldLista;
+                return [...oldLista, value];
+            });
+        }
+    }, [])
 
     return(
         <div>
-           <p>Dashboard</p>
+           <p>Lista</p>
 
-           <p>{nomeDoUsuario}</p>
+           <input onKeyDown={handleInputKeyDown} />
 
-           <p>Contador: {counterRef.current}</p>
-
-           <button onClick={() => counterRef.current++}>Somar</button>
-           <button onClick={() => console.log(counterRef.current)}>Log</button>
-
-           <button onClick={logout}>Logout</button>
-
-           <Link to="/entrar">Login</Link>
+           <ul>
+            {lista.map((value) => {
+                return <li key={value}>{value}</li>;
+            })}
+           </ul>
         </div>
     );
 }
